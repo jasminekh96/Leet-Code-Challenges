9e70191c355275bf4d1888db08d8ea72231492f5
Jasmine Khoury <jasminekh96@gmail.com>
2024-01-19 11:14:20 -0500

    70

---
+++ 70 Climbing Stairs/70.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if(n === 1) return 1;
+    let one = 1;
+    let two = 2;
+
+    for(let i = 3; i <= n; i++){
+        let three = one + two;
+        one = two;
+        two = three;
+    }
+    return two; 
+};
